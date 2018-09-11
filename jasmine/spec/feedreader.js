5be7f6f84125a1cb679d21b729b1724d91da964e
Benjamin Simonson <bfsimon@gmail.com>
2018-09-11 17:06:55 -0400

    begins to define a test ensures content changes when a new feed loads

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -105,10 +105,20 @@ $(function() {
 
     /* TODO: Write a new test suite named "New Feed Selection" */
     describe('New Feed Selection', function() {
+        const feed = document.querySelector('.feed');
 
         /* TODO: Write a test that ensures when a new feed is loaded
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+        beforeEach(function(done) {
+            loadFeed(0);
+            console.log(feed.children[0].innerText);
+            loadFeed(1, done);
+        });
+
+        it('changes content', function() {
+            console.log(feed.children[0].innerText);
+        });
     });
 }());
